25a44f0b7d540916635a4211749d583ce72e21d9
Abbey Jackson <abbey@abbeyjackson.ca>
2015-06-17 13:56:29 -0700

    Create FuelTableViewController and populate with array of fuel types

---
+++ DangerTreeAssessment/TreeClassTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TreeClassTableViewCell.h
+//  DangerTreeAssessment
+//
+//  Created by Abegael Jackson on 2015-06-17.
+//  Copyright (c) 2015 Abbey Jackson & Alain Kahwaji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TreeClassTableViewCell : UITableViewCell
+
+@end
---
+++ DangerTreeAssessment/TreeClassTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  TreeClassTableViewCell.m
+//  DangerTreeAssessment
+//
+//  Created by Abegael Jackson on 2015-06-17.
+//  Copyright (c) 2015 Abbey Jackson & Alain Kahwaji. All rights reserved.
+//
+
+#import "TreeClassTableViewCell.h"
+
+@implementation TreeClassTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
