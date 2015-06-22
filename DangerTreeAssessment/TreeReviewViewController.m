4ce4496e807c5297887609fa6b38650ffdfe04f3
Alain K <alain.kahwaji@gmail.com>
2015-06-22 00:55:29 -0700

    Complete if/else statements for TreeReviewController, still missing Management though

--- DangerTreeAssessment/TreeReviewViewController.m
+++ DangerTreeAssessment/TreeReviewViewController.m
@@ -16,12 +16,38 @@
 
 @property (nonatomic) IBOutlet UIScrollView *scrollView;
 
+//all trees
 @property (nonatomic) IBOutlet UILabel *latitudeLabel;
 @property (nonatomic) IBOutlet UILabel *longitudeLabel;
 @property (nonatomic) IBOutlet UILabel *speciesLabel;
 @property (nonatomic) IBOutlet UILabel *treeClassLabel;
 @property (nonatomic) IBOutlet UILabel *wildlifeValueLabel;
 @property (nonatomic) IBOutlet UILabel *commentsLabel;
+
+//LOD1 trees:
+@property (nonatomic) IBOutlet UILabel *insecureLabel;
+@property (nonatomic) IBOutlet UILabel *unstableLabel;
+@property (nonatomic) IBOutlet UILabel *leaningLabel;
+
+//LOD23 trees:
+@property (nonatomic) IBOutlet UILabel *hazardousTopLabel;
+@property (nonatomic) IBOutlet UILabel *deadLimbsLabel;
+@property (nonatomic) IBOutlet UILabel *witchesBroomLabel;
+@property (nonatomic) IBOutlet UILabel *splitTrunkLabel;
+@property (nonatomic) IBOutlet UILabel *stemDamageLabel;
+@property (nonatomic) IBOutlet UILabel *sloughingBarkLabel;
+@property (nonatomic) IBOutlet UILabel *cankersLabel;
+@property (nonatomic) IBOutlet UILabel *conksMushroomsLabel;
+@property (nonatomic) IBOutlet UILabel *treeLeanLabel;
+@property (nonatomic) IBOutlet UILabel *rootInspectionLabel;
+
+//LOD4 trees:
+@property (nonatomic) IBOutlet UILabel *ratingLabel;
+
+//Management (all trees)
+@property (nonatomic) IBOutlet UILabel *isDangerousLabel;
+@property (nonatomic) IBOutlet UILabel *management;
+@property (nonatomic) IBOutlet UILabel *comments;
 
 
 @end
@@ -32,7 +58,6 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     [self createScrollView];
-    
     [self updateLabels];
 }
 
@@ -83,41 +108,73 @@
 
 
     if ([tree.site.lod isEqualToString:kLODType1]){
-        NSLog(@"LOD is 1");
+        
+        self.insecureLabel = [[UILabel alloc] initWithFrame:CGRectMake(35, 270, 200, 50)];
+        self.insecureLabel.text = tree.insecure;
+        [self.view addSubview:self.insecureLabel];
+        
+        self.unstableLabel = [[UILabel alloc] initWithFrame:CGRectMake(35, 300, 200, 50)];
+        self.unstableLabel.text = tree.unstable;
+        [self.view addSubview:self.unstableLabel];
+        
+        self.leaningLabel = [[UILabel alloc] initWithFrame:CGRectMake(35, 330, 200, 50)];
+        self.leaningLabel.text = tree.leaning;
+        [self.view addSubview:self.leaningLabel];
+        
+        
     }else if ([tree.site.lod isEqualToString:kLODType23]){
-        NSLog(@"LOD is 23");
+
+        self.hazardousTopLabel = [[UILabel alloc] initWithFrame:CGRectMake(35, 270, 200, 50)];
+        self.hazardousTopLabel.text = tree.hazardousTop;
+        [self.view addSubview:self.hazardousTopLabel];
+        
+        self.deadLimbsLabel = [[UILabel alloc] initWithFrame:CGRectMake(35, 300, 200, 50)];
+        self.deadLimbsLabel.text = tree.deadLimbs;
+        [self.view addSubview:self.deadLimbsLabel];
+        
+        self.witchesBroomLabel = [[UILabel alloc] initWithFrame:CGRectMake(35, 330, 200, 50)];
+        self.witchesBroomLabel.text = tree.witchesBroom;
+        [self.view addSubview:self.witchesBroomLabel];
+        
+        self.splitTrunkLabel = [[UILabel alloc] initWithFrame:CGRectMake(35, 360, 200, 50)];
+        self.splitTrunkLabel.text = tree.splitTrunk;
+        [self.view addSubview:self.splitTrunkLabel];
+        
+        self.stemDamageLabel = [[UILabel alloc] initWithFrame:CGRectMake(35, 390, 200, 50)];
+        self.stemDamageLabel.text = tree.stemDamage;
+        [self.view addSubview:self.stemDamageLabel];
+        
+        self.sloughingBarkLabel = [[UILabel alloc] initWithFrame:CGRectMake(35, 420, 200, 50)];
+        self.sloughingBarkLabel.text = tree.sloughingBark;
+        [self.view addSubview:self.sloughingBarkLabel];
+        
+        self.cankersLabel = [[UILabel alloc] initWithFrame:CGRectMake(35, 450, 200, 50)];
+        self.cankersLabel.text = tree.cankers;
+        [self.view addSubview:self.cankersLabel];
+        
+        self.conksMushroomsLabel = [[UILabel alloc] initWithFrame:CGRectMake(35, 480, 200, 50)];
+        self.conksMushroomsLabel.text = tree.conksMushrooms;
+        [self.view addSubview:self.conksMushroomsLabel];
+        
+        self.treeLeanLabel = [[UILabel alloc] initWithFrame:CGRectMake(35, 510, 200, 50)];
+        self.treeLeanLabel.text = tree.treeLean;
+        [self.view addSubview:self.treeLeanLabel];
+        
+        self.rootInspectionLabel = [[UILabel alloc] initWithFrame:CGRectMake(35, 540, 200, 50)];
+        self.rootInspectionLabel.text = tree.rootInspection;
+        [self.view addSubview:self.rootInspectionLabel];
+
+        
     }else if ([tree.site.lod isEqualToString:kLODType4]){
-        NSLog(@"LOD is 4");
+        self.ratingLabel = [[UILabel alloc] initWithFrame:CGRectMake(35, 270, 200, 50)];
+        self.ratingLabel.text = tree.rating;
+        [self.view addSubview:self.ratingLabel];
     }
     
 }
-    
-//    // LOD1 trees:
-//    
-//    tree.insecure;
-//    tree.unstable;
-//    tree.leaning;
-//    
-//    // tree properties for LOD23 trees:
-//    tree.hazardousTop;
-//    tree.deadLimbs;
-//    tree.witchesBroom;
-//    tree.splitTrunk;
-//    tree.stemDamage;
-//    tree.sloughingBark;
-//    tree.cankers;
-//    tree.conksMushrooms;
-//    tree.treeLean;
-//    tree.rootInspection;
-//    
-//    // tree properties for LOD4 trees:
-//    tree.rating;
-//    
-//    // all trees:
-//    tree.isDangerous;
-//    tree.management;
-//    tree.comments;
-//}
+
+
+
 
 #pragma mark - Navigation
 
