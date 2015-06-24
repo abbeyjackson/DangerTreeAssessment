8d7ba2907593149051df0e0ad39cb6a08b83b8ac
Abbey Jackson <abbey@abbeyjackson.ca>
2015-06-23 17:13:35 -0700

    Data List tab bar button always shows root view controller (data list); LOD1 and LOD23 will set safe/dangerous segment on Management VC to Dangerous if any segment was set to Dangerous

---
+++ DangerTreeAssessment/TabBarController.h
@@ -0,0 +1,13 @@
+//
+//  TabBarController.h
+//  DangerTreeAssessment
+//
+//  Created by Abegael Jackson on 2015-06-23.
+//  Copyright (c) 2015 Abbey Jackson & Alain Kahwaji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabBarController : UITabBarController
+
+@end
